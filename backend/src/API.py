a7248fa89c5ebc8a43092a58573b04c3618abdd1
CS student <me@DebianXfce23F.cs.uoregon.edu>
2026-05-28 09:59:56 -0700

    added dummy value returns for api calls

--- backend/src/API.py
+++ backend/src/API.py
@@ -4,9 +4,14 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import os
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
+
+mongo_host = os.environ.get('DB_HOST', 'db')
+client = MongoClient(mongo_host, 27017)
+db = client.healthdiary
 
 """
 @app.route("/")
@@ -15,14 +20,40 @@ def home():
     return jsonify({"status": "ok"})
 """
 
+def generate_id():
+
+    id = 0
+
+    return id
+
 @app.route("/api/store_user_log")
 def store_user_log():
+
     
-    return jsonify({"status": "ok"})
+    
+    return jsonify({"message": "entry added to database"})
 
 @app.route("/api/fetch_user_log")
 def fetch_user_log():
-    return jsonify({"status": "ok"})
+
+    ##return dummy values
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # points to src/
+    file_path = os.path.join(base_dir, '..', 'db', 'example_data', 'journal_entry.example.json')
+
+    with open(file_path) as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route("/api/get_user_prefs")
+def get_user_prefs():
+
+    ##return dummy values
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # points to src/
+    file_path = os.path.join(base_dir, '..', 'db', 'example_data', 'preferences.eexample.json')
+
+    with open(file_path) as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
