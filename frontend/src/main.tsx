7926512b3cbfa042c46bd2a5cf158b2657112e7f
KaeganKoski <kizaegan@gmail.com>
2026-05-28 15:14:52 -0800

    Added empty pages for navbar to link toa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
 
 import App from "./pages/App.tsx";
+import Calendar from "./pages/Calendars.tsx";
+import Settings from "./pages/Settings.tsx";
+import Diary from "./pages/Diary.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -13,6 +16,9 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
   },
+  { path: "/calendar", element: <Calendar /> },
+  { path: "/settings", element: <Settings /> },
+  { path: "/diary", element: <Diary /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
